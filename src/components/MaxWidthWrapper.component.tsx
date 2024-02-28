e6945114c243c56bf89f2bd66069b0cca44495a1
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-02-28 18:13:16 +0530

    initital commit

---
+++ src/components/MaxWidthWrapper.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {cn} from '@/lib/utils.lib';
+export  function MaxWidthWrapper({className,children}:{className?:string,children:React.ReactNode}) {
+  return (
+    <div className={cn('max-w-screen-xl mx-auto w-full px-2.5 md:px-20',className)}>
+        {children}
+    </div>
+  )
+}
