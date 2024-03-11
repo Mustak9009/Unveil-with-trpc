833621a094119cbd3a1bacd55b1db3c5ffae99a9
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-03-11 09:11:54 +0530

    Work on dilalog box - morning - 11-03

--- src/components/UploadButton.component.tsx
+++ src/components/UploadButton.component.tsx
@@ -2,6 +2,24 @@
 import React from "react";
 import {Dialog,DialogTrigger,DialogContent} from '@/components/ui/dialog';
 import { Button } from "./ui/button";
+import DropZone from 'react-dropzone';
+const UploadDropZone = () =>{
+  return(
+    <DropZone multiple={false} onDrop={(acceptedFile)=>console.log(acceptedFile)}>
+      {({getRootProps,getInputProps,acceptedFiles})=>(
+        <div {...getRootProps()} className="h-64 m-4 border border-dashed border-gray-300 rounded-lg">
+          <div className="flex justify-center items-center h-full w-full">
+            <label htmlFor="dropzone-file" className="flex flex-col justify-center items-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+              <div className="flex items-center justify-center flex-col pt-5 pb-6">
+                abc
+              </div>
+            </label>
+          </div>
+        </div>
+      )}
+    </DropZone>
+  )
+}
 export default function UploadButton() {
   const [isOpen, setIsOpen] = React.useState<boolean>(false);
   return (
@@ -9,8 +27,8 @@ export default function UploadButton() {
       <DialogTrigger asChild>
         <Button>Upload PDF</Button>
       </DialogTrigger>
-      <DialogContent>
-        avc
+      <DialogContent className="w-11/12 sm:max-w-md">
+        <UploadDropZone/>
       </DialogContent>
     </Dialog>
   )
