a7da98c96fdb130db3649d0cfb95c954295abd26
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-04-02 09:32:37 +0530

    test 002 | fix

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -2,8 +2,9 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "@/trpc/client";
 import { Loader2 } from "lucide-react";
-import { useEffect, Suspense } from "react";
-export default function Page() {
+import { Suspense, useEffect } from "react";
+
+const Loader = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
@@ -17,16 +18,20 @@ export default function Page() {
     }
   }, [data, error, router, origin]);
   return (
-    <Suspense>
-      <div className="w-full mt-24 flex justify-center">
-        <div className="flex flex-col items-center gap-2 ">
-          <Loader2 className="w-8 h-8 animate-spin text-zinc-800" />
-          <h3 className="font-semibold text-xl">
-            Setting up your account . . .
-          </h3>
-          <p>You will be redirect automatically</p>
-        </div>
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2 ">
+        <Loader2 className="w-8 h-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account . . .</h3>
+        <p>You will be redirect automatically</p>
       </div>
+    </div>
+  );
+};
+
+export default function Page() {
+  return (
+    <Suspense>
+      <Loader />
     </Suspense>
   );
 }
