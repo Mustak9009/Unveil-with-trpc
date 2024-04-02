a089f1f858efeefa4dd40f33e8908b127405bf52
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-04-02 09:26:34 +0530

    test 001

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,28 +1,32 @@
-'use client'
+"use client";
 import { useRouter, useSearchParams } from "next/navigation";
-import {trpc} from '@/trpc/client';
+import { trpc } from "@/trpc/client";
 import { Loader2 } from "lucide-react";
-import { useEffect } from "react";
-export default  function Page() {
+import { useEffect, Suspense } from "react";
+export default function Page() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const origin = searchParams.get('origin')
-  const {data,error} = trpc.authCallBack.useQuery()
-  useEffect(()=>{
-    if(error?.data?.code === 'UNAUTHORIZED'){
-      router.push('/sign-in')
+  const origin = searchParams.get("origin");
+  const { data, error } = trpc.authCallBack.useQuery();
+  useEffect(() => {
+    if (error?.data?.code === "UNAUTHORIZED") {
+      router.push("/sign-in");
     }
-    if(data?.success){
-      router.push(origin ? `/${origin}` : '/admin')
+    if (data?.success) {
+      router.push(origin ? `/${origin}` : "/admin");
     }
-  },[data,error,router,origin])
-  return(
-    <div className="w-full mt-24 flex justify-center">
-      <div className="flex flex-col items-center gap-2 ">
-        <Loader2 className="w-8 h-8 animate-spin text-zinc-800"/>
-        <h3 className="font-semibold text-xl">Setting up your account . . .</h3>
-        <p>You will be redirect automatically</p>
+  }, [data, error, router, origin]);
+  return (
+    <Suspense>
+      <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2 ">
+          <Loader2 className="w-8 h-8 animate-spin text-zinc-800" />
+          <h3 className="font-semibold text-xl">
+            Setting up your account . . .
+          </h3>
+          <p>You will be redirect automatically</p>
+        </div>
       </div>
-    </div>
-  )
+    </Suspense>
+  );
 }
