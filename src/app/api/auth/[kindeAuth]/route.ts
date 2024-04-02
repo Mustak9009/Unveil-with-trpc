16b3221cd58f4a4e376a342f1dec4804b47a0d19
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-04-02 08:44:50 +0530

    fix test 003

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,8 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
 import { NextRequest } from 'next/server'
 
-export async function GET(request: NextRequest,{  params }: {params:{kindeAuth:string}}) {
+export async function GET(request: NextRequest,{  params }: {params:{kindeAuth:string}}) { 
   const endpoint = params.kindeAuth
+  //@ts-ignore
   return handleAuth(request, endpoint)
 }
